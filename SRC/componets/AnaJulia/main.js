90d883c062db6423b9c13724232278af7c45b0f5
0anajuliaf <ana.santos3092@etec.sp.gov.br>
2024-03-21 08:35:10 -0300

    prova DDM lado A

---
+++ SRC/componets/AnaJulia/main.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AnaJulia() {
+  return (
+    <View style={styles.container}>
+      <Text>Ana Julia Ferreira dos Santos</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ SRC/componets/AnnaBeatriz/main.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AnnaBeatriz() {
+  return (
+    <View style={styles.container}>
+      <Text>Anna Beatriz Vicente de Souza</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
